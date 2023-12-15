171a203512f793675e2456614b0d4e42a1bf7838
Arvind Kumar <aarviiyadav19@gmail.com>
2023-12-15 12:10:50 +0530

    router crash course

---
+++ 07_react_router/src/components/Github/Github.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+function Github() {
+    const [data, setData] = useState([])
+    useEffect(() => {
+      fetch('https://api.github.com/users/arvindkryadav1')
+      .then(response => response.json())
+      .then(data =>{
+        console.log(data);
+        setData(data)
+      })
+    }, [])
+    
+  return (
+    <div className='text-center m-4 bg-gray-500 text-white p-4 text-3xl'>Github Followers:{data.followers}
+     <img src={data.avatar_url} alt='Git picture' width={300}/>
+    </div>
+  )
+}
+
+export default Github
