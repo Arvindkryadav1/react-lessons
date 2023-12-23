0c9beab70c63b88826b5a29425abff33ebc591af
Arvind Kumar <aarviiyadav19@gmail.com>
2023-12-23 17:19:55 +0530

    Adding form and slug Values

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container"
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Login"
+import Button from "./Button"
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Post from "./Post"
+import Postform from "./post-form/Postform";
+import Postcard from "./Postcard"
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Input,
+    Button,
+    RTE,
+    Signup,
+    Login,
+    Post,
+    Postform,
+    Postcard,
+
+}
