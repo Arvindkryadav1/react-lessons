f409136ca2ced2d34ff5db5161b9e142e4ecbec6
Arvind Kumar <aarviiyadav19@gmail.com>
2023-12-24 20:16:24 +0530

    debug and CORS

--- 12MegaBlog/src/components/Footer/Footer.jsx
+++ 12MegaBlog/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Logo from '../Logo'
 
 function Footer() {
@@ -14,7 +14,7 @@ function Footer() {
                             </div>
                             <div>
                                 <p className="text-sm text-gray-600">
-                                    &copy; Copyright 2023. All Rights Reserved by Arvind.
+                                    &copy; Copyright 2023. All Rights Reserved by DevUI.
                                 </p>
                             </div>
                         </div>
--- 12MegaBlog/src/store/authSlice.js
+++ 12MegaBlog/src/store/authSlice.js
@@ -1,14 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    status: false,
-    userData: null,
+    status : false,
+    userData: null
 }
 
 const authSlice = createSlice({
     name: "auth",
     initialState,
-    reducers:{
+    reducers: {
         login: (state, action) => {
             state.status = true;
             state.userData = action.payload.userData;
@@ -17,7 +17,9 @@ const authSlice = createSlice({
             state.status = false;
             state.userData = null;
         }
-    }
+     }
 })
-export const {login, logout} = authSlice.actions
+
+export const {login, logout} = authSlice.actions;
+
 export default authSlice.reducer;
